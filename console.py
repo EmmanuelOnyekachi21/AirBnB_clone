7fb3707ff9a1d330121de85a5923ef53f60d2999
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-05-16 10:57:45 +0100

    Task 6 - Adjusted console.py to be compliant to the pycodestyle

--- console.py
+++ console.py
@@ -6,24 +6,56 @@ Command line interpreter (CLI) using the CMD module.
 import cmd
 import sys
 
+
 class HBNBCommand(cmd.Cmd):
     """
-    Contain the entry point of the command interpreter.
+    Command-line interpreter for the HBNB application.
+
+    Attributes:
+        prompt (str): The prompt string displayed to the user.
+
+    Methods:
+        do_quit(arg):   Handles the 'quit' command to exit the program.
+        emptyline():    Does nothing when an
+                        empty line + ENTER shouldn’t execute anything
+        do_EOF(arg):    Handles the end-of-file condition to gracefully
+                        exit the program.
     """
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program
+        """
+         Quit command to exit the program.
+
+        Args:
+            arg:    Unused argument (required by cmd.Cmd).
+
+        Returns:
+            None
+
+        Raises:
+            SystemExit:     Exits the program with a status code of 0.
         """
         sys.exit()
 
     def emptyline(self):
-        """Does nothing when an empty line + ENTER shouldn’t execute anything
+        """
+        Does nothing when an empty line + ENTER shouldn’t execute anything
         """
         pass
 
     def do_EOF(self, arg):
-        """Handles end-of-file (EOF).
+        """
+        Handles end-of-file (EOF) to gracefully exit the program.
+
+        Args:
+            arg: Unused argument (required by cmd.Cmd).
+
+        Returns:
+            bool: True to indicate the end-of-file condition.
+
+        Notes:
+            Typing CTRL+D (UNIX-like systems) triggers EOF.
         """
         return True
 
