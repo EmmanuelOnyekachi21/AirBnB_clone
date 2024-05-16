6b16c8013360e53b154f1692c66cbb0af124c893
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-05-16 09:57:01 +0100

    Added Code ranging from Task 3 - 6

---
+++ models/base_model.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+"""
+defines all common attributes/methods for other classes
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """
+    defines all common attributes/methods for other classes.
+
+    Attributes:
+        id (str):               assigned with an uuid when
+                                    an instance is created
+        created_at (datetime):     assign with the current datetime when
+                                    an instance is created.
+        updated_at (datetime):     assign with the current datetime
+                                    when an instance is created.  It will
+                                    be updated every time you change the object
+    Methods:
+        save(self):     updates the public instance attribute `updated_at`
+                        with the current datetime
+        to_dict(self):  returns a dictionary containing all keys/values of
+                        `__dict__` of the instance
+    """
+    TIME_FORM = "%Y-%m-%dT%H:%M:%S.%f"
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes the BaseModel instance.
+
+        Arguments:
+            kwargs:             to re-create an instance with a `dict`
+                                representation.
+        Attributes:
+        id (str):               assigned with an uuid when
+                                    an instance is created
+        created_at (datetime):     assign with the current datetime when
+                                    an instance is created.
+        updated_at (datetime):     assign with the current datetime
+                                    when an instance is created.  It will
+                                    be updated every time you change the object
+
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(
+                            self, key, datetime.strptime(value, self.TIME_FORM)
+                            )
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        Returns a string represnetation of the BaseModel instance.
+
+        Returns:
+            str:    A string containing the class name, ID,
+                    and dictionary representation.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        updates the public instance attribute `updated_at` with
+        the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of
+        `__dict__` of the instance.
+
+        Returns:
+            dict: A dictionary containing all keys/values of
+            `__dict__` of the instance.
+        """
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+
+        for key, value in dictionary.items():
+            if isinstance(value, datetime):
+                dictionary[key] = value.isoformat()
+        return dictionary
---
+++ models/engine/file_storage.py
@@ -0,0 +1,63 @@
+"""
+serializes instances to a JSON file and deserializes JSON file to instances
+"""
+
+
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    serializes instances to a JSON file and deserializes JSON file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    models = {
+        'BaseModel': BaseModel,
+        }
+
+    def all(self):
+        """
+        Returns the dictionary `__objects`
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        objects = {}
+
+        for key, value in self.__objects.items():
+            objects[key] = value.to_dict()
+
+        with open(self.__file_path, "w") as file:
+            json.dump(objects, file, indent=2)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.)
+        """
+        if (
+                (os.path.exists(self.__file_path)) and
+                (os.path.getsize(self.__file_path) > 0)):
+            with open(self.__file_path, "r") as file:
+                data = json.load(file)
+
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    cls = self.models[class_name]
+                    obj = cls(**value)
+                    self.__objects[key] = obj
