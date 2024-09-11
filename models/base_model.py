f28db664fad01ca0d82987b6aafcb658a5d5ab45
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-09-11 16:23:04 +0100

    Task 18

--- models/base_model.py
+++ models/base_model.py
@@ -1,91 +1,39 @@
 #!/usr/bin/python3
 """
-defines all common attributes/methods for other classes
+BaseModel.
 """
+
 
 import uuid
 from datetime import datetime
-import models
-
 
 class BaseModel:
     """
-    defines all common attributes/methods for other classes.
-
-    Attributes:
-        id (str):               assigned with an uuid when
-                                    an instance is created
-        created_at (datetime):     assign with the current datetime when
-                                    an instance is created.
-        updated_at (datetime):     assign with the current datetime
-                                    when an instance is created.  It will
-                                    be updated every time you change the object
-    Methods:
-        save(self):     updates the public instance attribute `updated_at`
-                        with the current datetime
-        to_dict(self):  returns a dictionary containing all keys/values of
-                        `__dict__` of the instance
+    Class.
     """
-    TIME_FORM = "%Y-%m-%dT%H:%M:%S.%f"
-
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
-        Initializes the BaseModel instance.
-
-        Arguments:
-            kwargs:             to re-create an instance with a `dict`
-                                representation.
-        Attributes:
-        id (str):               assigned with an uuid when
-                                    an instance is created
-        created_at (datetime):     assign with the current datetime when
-                                    an instance is created.
-        updated_at (datetime):     assign with the current datetime
-                                    when an instance is created.  It will
-                                    be updated every time you change the object
-
+        Init.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, self.TIME_FORM)
-                if key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """
-        Returns a string represnetation of the BaseModel instance.
-
-        Returns:
-            str:    A string containing the class name, ID,
-                    and dictionary representation.
+        Str
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        updates the public instance attribute `updated_at` with
-        the current datetime
-
-        Return:
-            None
+        Save
         """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of
-        `__dict__` of the instance.
-
-        Returns:
-            dict: A dictionary containing all keys/values of
-            `__dict__` of the instance.
+        to_dict
         """
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
@@ -93,4 +41,5 @@ class BaseModel:
         for key, value in dictionary.items():
             if isinstance(value, datetime):
                 dictionary[key] = value.isoformat()
+
         return dictionary
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,37 +1,43 @@
 """
-Test cases for the base_model module.
+Tests
 """
 
-import unittest
+
+import unittests
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from datetime import datetime
-import os
+
 
 
 class TestBaseModel(unittest.TestCase):
     """
-    Test case for the BaseModel class
+    Y
     """
-
     def setUp(self):
-        """Common setup for test cases."""
+        """
+        Setup
+        """
         self.model = BaseModel()
 
     def tearDown(self):
-        """Common cleanup for test cases."""
+        """
+        TD
+        """
         del self.model
 
     def test_instance_creation(self):
-        """Test creation of BaseModel instance."""
+        """
+        TIC
+        """
         self.assertIsInstance(self.model, BaseModel)
         self.assertIsInstance(self.model.id, str)
         self.assertIsInstance(self.model.created_at, datetime)
         self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_save_method(self):
-        """Test save method."""
-        initial_updated_at = self.model.updated_at
+        """
+        test
+        """
+        initial_update = self.model.updated_at
         self.model.save()
         self.assertNotEqual(self.model.updated_at, initial_updated_at)
 
@@ -49,129 +55,3 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', model_dict)
         self.assertEqual(
                 model_dict['updated_at'], self.model.updated_at.isoformat())
-
-
-class TestBaseModel_kwargs(unittest.TestCase):
-    """
-    A unittest for the BaseModel class init with kwargs
-    """
-
-    def setUp(self):
-        """Common setup for test cases."""
-        self.model = BaseModel()
-
-    def tearDown(self):
-        """Common cleanup for test cases."""
-        del self.model
-
-    def test_init(self):
-        """
-        Test initialization of BaseModel instance.
-        """
-        self.assertIsInstance(self.model.id, str)
-        self.assertIsInstance(self.model.created_at, datetime)
-        self.assertIsInstance(self.model.updated_at, datetime)
-
-    def test_str(self):
-        """
-        Test string representation of BaseModel instance.
-        """
-        string_repr = str(self.model)
-        self.assertIn("[BaseModel]", string_repr)
-        self.assertIn(self.model.id, string_repr)
-        self.assertIn(str(self.model.__dict__), string_repr)
-
-    def test_save(self):
-        """
-        Test saving functionality of BaseModel instance.
-        """
-        old_updated_at = self.model.updated_at
-        self.model.save()
-        self.assertNotEqual(old_updated_at, self.model.updated_at)
-
-    def test_to_dict(self):
-        """
-        Test conversion of BaseModel instance to dictionary.
-        """
-        model_dict = self.model.to_dict()
-        self.assertIsInstance(model_dict, dict)
-        self.assertIn('__class__', model_dict)
-        self.assertIn('id', model_dict)
-        self.assertIn('created_at', model_dict)
-        self.assertIn('updated_at', model_dict)
-        self.assertEqual(model_dict['__class__'], 'BaseModel')
-        self.assertEqual(model_dict['id'], self.model.id)
-        self.assertEqual(
-                model_dict['created_at'], self.model.created_at.isoformat())
-        self.assertEqual(
-                model_dict['updated_at'], self.model.updated_at.isoformat())
-
-    def test_init_from_dict(self):
-        """
-        Test initialization of BaseModel instance from dictionary.
-        """
-        model_dict = self.model.to_dict()
-        new_model = BaseModel(**model_dict)
-        self.assertEqual(new_model.id, self.model.id)
-
-class TestFileStorage(unittest.TestCase):
-    """Test cases for the FileStorage class."""
-
-    def setUp(self):
-        """Set up the test environment.
-        """
-        self.storage = FileStorage()
-        self.base_model = BaseModel()
-
-    def tearDown(self):
-        """Tear Down the test environment."""
-        del self.storage
-        self.base_model
-
-    def test_all(self):
-        """
-        Test the all() method.
-        """
-        all_objects = self.storage.all()
-        self.assertIsInstance(all_objects, dict)
-
-    def test_new(self):
-        """
-        Test the new() method.
-        """
-        self.storage.new(self.base_model)
-        all_objects = self.storage.all()
-        self.assertIn('BaseModel.' + self.base_model.id, all_objects)
-        self.assertEqual(
-                all_objects['BaseModel.' + self.base_model.id],
-                self.base_model)
-
-    def test_save_reload(self):
-        """Test the save() and reload() methods."""
-        # Save the BaseModel Instance
-        self.storage.new(self.base_model)
-        self.storage.save()
-
-        # Reload the data and check if BaseModel instance is present
-        new_storage = FileStorage()
-        new_storage.reload()
-        all_objects = new_storage.all()
-        self.assertIn('BaseModel.' + self.base_model.id, all_objects)
-        self.assertIsInstance(
-                all_objects['BaseModel.' + self.base_model.id], BaseModel)
-
-    def test_save_file_exists(self):
-        """Test the save() method when the file exists."""
-        self.storage.save()
-        self.assertTrue(os.path.exists(self.storage._FileStorage__file_path))
-
-    def test_save_file_not_exists(self):
-        """Test the save() method when the file does not exist."""
-        os.remove(self.storage._FileStorage__file_path)
-        self.assertFalse(os.path.exists(self.storage._FileStorage__file_path))
-        self.storage.save()
-        self.assertTrue(os.path.exists(self.storage._FileStorage__file_path))
-
-
-if __name__ == '__main__':
-    unittest.main()
