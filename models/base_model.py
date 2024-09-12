5701c2e34b22b3fb3b320247025c137752738859
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-09-12 12:02:10 +0100

    3

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,22 @@ class BaseModel:
     """
     Class.
     """
-    def __init__(self):
+    TIME_FORM = "%Y-%m-%dT%H:%M:%S.%f"
+
+    def __init__(self, *args, **kwargs):
         """
         Init.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, BaseModel.TIME_FORM)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,8 +3,9 @@ Tests
 """
 
 
-import unittests
+import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 
@@ -37,7 +38,7 @@ class TestBaseModel(unittest.TestCase):
         """
         test
         """
-        initial_update = self.model.updated_at
+        initial_updated_at = self.model.updated_at
         self.model.save()
         self.assertNotEqual(self.model.updated_at, initial_updated_at)
 
@@ -55,3 +56,70 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('updated_at', model_dict)
         self.assertEqual(
                 model_dict['updated_at'], self.model.updated_at.isoformat())
+
+
+class TestBaseModel_Kwargs(unittest.TestCase):
+    """
+    IIII
+    """
+    def setUp(self):
+        """Common setup for test cases."""
+        self.model = BaseModel()
+
+    def tearDown(self):
+        """Common cleanup for test cases."""
+        del self.model
+
+    def test_init(self):
+        """
+        Test initialization of BaseModel instance.
+        """
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
+
+    def test_str(self):
+        """
+        go
+        """
+        string_repr = str(self.model)
+        self.assertIn("[BaseModel]", string_repr)
+        self.assertIn(self.model.id, string_repr)
+        self.assertIn(str(self.model.__dict__), string_repr)
+
+    def test_save(self):
+        """
+        Test saving functionality of BaseModel instance.
+        """
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(old_updated_at, self.model.updated_at)
+
+    def test_to_dict(self):
+        """
+        Test conversion of BaseModel instance to dictionary.
+        """
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertIn('__class__', model_dict)
+        self.assertIn('id', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], self.model.id)
+        self.assertEqual(
+                model_dict['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(
+                model_dict['updated_at'], self.model.updated_at.isoformat())
+
+    def test_init_from_dict(self):
+        """
+        Te
+        """
+        model_dict = self.model.to_dict()
+        new_model = BaseModel(**model_dict)
+        self.assertEqual(new_model.id, self.model.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
